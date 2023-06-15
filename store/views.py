67bc02ebbd2f78f62269f0223893a8cb962e770a
suprit-kumar <supritk16@gmail.com>
2023-06-15 21:40:41 +0530

    feat: pagination added

--- store/views.py
+++ store/views.py
@@ -5,6 +5,7 @@ from category import models as cat_model
 from carts.models import CartItem
 from carts.views import _cart_id
 import traceback
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 def store(request, slug=None):
     specific_category = None
@@ -12,15 +13,25 @@ def store(request, slug=None):
     if slug:
         specific_category = get_object_or_404(cat_model.Category, cat_slug=slug)
 
-        products = models.Product.objects.values('product_name', 'product_price', 'product_image','product_id',
-                                                 'product_slug','product_category__cat_slug',).filter(
+        products = models.Product.objects.values('product_name', 'product_price', 'product_image', 'product_id',
+                                                 'product_stock',
+                                                 'product_slug', 'product_category__cat_slug', ).filter(
             is_available=True, product_category=specific_category).order_by('-product_modified_date')
+        display_count = 2
     else:
-        products = models.Product.objects.values('product_name', 'product_price', 'product_image','product_id',
-                                                 'product_slug','product_category__cat_slug',).filter(
+        products = models.Product.objects.values('product_name', 'product_price', 'product_image', 'product_id',
+                                                 'product_stock',
+                                                 'product_slug', 'product_category__cat_slug', ).filter(
             is_available=True).order_by('-product_modified_date')
+        display_count = 6
+    paginator = Paginator(products, display_count)
+    page = request.GET.get("page")
+    paged_products = paginator.get_page(page)
+    product_count = products.count()
+
     context = {
-        'products': products,
+        'products': paged_products,
+        'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
 
@@ -28,9 +39,9 @@ def store(request, slug=None):
 def product_detail(request, slug, product_slug):
     try:
         single_product = models.Product.objects.get(product_category__cat_slug=slug, product_slug=product_slug)
-        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request),product=single_product).exists()
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
-    context = {'single_product':single_product,'in_cart':in_cart}
+    context = {'single_product': single_product, 'in_cart': in_cart}
 
-    return render(request, 'store/product_detail.html',context)
+    return render(request, 'store/product_detail.html', context)
