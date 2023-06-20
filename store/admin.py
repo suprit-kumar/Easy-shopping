ca3dd70464cc947da69177c74833eb86994b56f6
suprit-kumar <supritk16@gmail.com>
2023-06-20 14:38:16 +0530

    feat: add product image gallery

--- store/admin.py
+++ store/admin.py
@@ -12,7 +12,7 @@ class ProductGalleryInline(admin.TabularInline):
 class ProductAdmin(admin.ModelAdmin):
   prepopulated_fields = {"product_slug": ("product_name",)}
   list_display = ('product_name','product_price','product_stock','product_category','product_modified_date','is_available')
-
+  inlines = [ProductGalleryInline]
 
 class VariationAdmin(admin.ModelAdmin):
   list_display = ('product', 'variation_category', 'variation_value', 'is_active', 'created_date',)
